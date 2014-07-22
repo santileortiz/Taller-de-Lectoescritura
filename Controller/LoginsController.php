a30b4090e04c22d6d408512f106908390ad01e9b
Santiago Leon <santileortiz@gmail.com>
2014-07-21 20:33:36 -0500

    ahora se pueden editar y borrar logins

--- Controller/LoginsController.php
+++ Controller/LoginsController.php
@@ -17,5 +17,30 @@ class LoginsController extends AppController {
 			endif;
 		endif;
 	}
+
+	public function edit($id = null){
+		$this->Login->id = $id;
+		if($this->request->is('get')):
+			$this->request->data = $this->Login->read();
+		else: //peticion no es get
+			if($this->Login->save($this->request->data)):
+				$this->Session->setFlash('¡Login editado!', 'default', array('class'=>'custom_success'));
+				$this->redirect( array('action' => 'index') );
+			else:
+				$this->Session->setFlash('Error al guardar los cambios...');
+			endif;
+		endif;
+	}
+
+	public function delete($id){
+		if($this->request->is('get')):
+			throw new MethodNotAllowedException('Esto es hack');
+		else:
+			$this->Session->setFlash('¡Login eliminado!', 'default', array('class'=>'custom_success'));
+			if($this->Login->delete($id)):
+				$this->redirect( array('action' => 'index') );
+			endif;
+		endif;
+	}
 }
 ?>
--- View/Logins/edit.ctp
+++ View/Logins/edit.ctp
@@ -1,11 +1,9 @@
-<h2>Editar tutor</h2>
+<h2>Editar login</h2>
 <?php
-		echo $this->Form->create('Tutor', array('action' => 'edit'));
-		echo $this->Form->input('tec_id', array('type' => 'text', 'label' => 'Matrícula'));
-		echo $this->Form->input('name');
-		echo $this->Form->input('last_name');
-		echo $this->Form->input('career');
-		echo $this->Form->input('cell_phone');
+		echo $this->Form->create('Login', array('action' => 'edit'));
+		echo $this->Form->input('user', array('type' => 'text', 'label' => 'Matrícula'));
+		echo $this->Form->input('passowrd');
+		echo $this->Form->input('type');
 		echo $this->Form->end('Guardar cambios');
 ?>
 <a href="../" class="btnRegresa">Salir sin cambios</a></li>
